bba1cccf3bf6505229e508ff3c067aa1563da0ca
panpalli <memduhpanpalli@gmail.com>
2022-12-20 23:59:59 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let js = 'amazing';
+if (js === 'amazing') alert("Javascript is FUN");
+
+40 + 6 + 23 - 7;
+console.log(40 + 6 + 23 - 7);
+console.log('AREN');
+
+let firstName = "Memduh";
+
+console.log(firstName);  //Variable
+console.log(firstName);  //Variable
+console.log(firstName);  //Variable
+
+let aren$Memduh = 'ArMe';
+console.log(aren$Memduh);
+
+let country = "Germany";
+
+console.log(country);
+
+let continent = 'Europe';
+
+console.log(continent);
+
+let population = '83 million';
+
+console.log(population);
